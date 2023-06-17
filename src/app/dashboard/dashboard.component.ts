04afe13278df3ef01f9e1bd3f0833e187e71e005
agiram <agiram@saisystems.com>
2023-06-17 11:22:06 +0530

    array issue resolved

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { UsersService } from '../users/users.service';
-import * as collegeData from './2019.json'
+import  collegeData from './2019.json'
 import { FormBuilder, FormGroup } from '@angular/forms';
 @Component({
   selector: 'app-dashboard',
@@ -12,21 +12,21 @@ export class DashboardComponent implements OnInit {
   collegeForm: FormGroup;
   
   searchedColleges:any[]=[];
-  collegeData:any[]
+  
   constructor(private http: HttpClient,
     private userService : UsersService,
     private formBuilder:FormBuilder) { }
-    college2019 : string = '/assets/2019.json'
+    college2019 :any[]= collegeData
 
   ngOnInit(): void {
     this.collegeForm = this.formBuilder.group({
       collegeName:['']
     })
     
-    this.http.get(this.college2019).subscribe((res:any)=>{
-      this.collegeData = res;
-      console.log(this.collegeData)
-    })
+    // this.http.get(this.college2019).subscribe((res:any)=>{
+    //   this.collegeData = res;
+    //   console.log(this.collegeData)
+    // })
   }
 
  get f() { return this.collegeForm.controls; }
@@ -34,7 +34,7 @@ export class DashboardComponent implements OnInit {
 searchCollege(){
   console.log(this.collegeForm.value.collegeName)
   this.searchedColleges=[]
-  this.collegeData.forEach((college:any) => {
+  this.college2019.forEach((college:any) => {
     if(college.Institute.includes(this.collegeForm.value.collegeName)){
       this.searchedColleges.push(college);
     }
